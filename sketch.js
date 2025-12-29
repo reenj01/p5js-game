d8e6ab73efd6848f303e1a8bb7bd9123e37a5d99
reenj01 <ireenjeang0726@gmail.com>
2025-12-29 15:18:29 -0800

    add video capture

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
+let capture;
+
 function setup() {
   createCanvas(600, 600);
+  capture = createCapture(VIDEO);
+  capture.size(600,600);
+  capture.hide();
 }
 
 function draw() {
-  background(0);
+  background(220);
+  image(capture, 0, 0, 600, 600);
 }
