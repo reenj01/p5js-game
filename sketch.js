50fd4351d5b9eefba00a042099f2680a1ae5d6fc
reenj01 <ireenjeang0726@gmail.com>
2025-12-29 16:50:10 -0800

    detect index and thumb

--- sketch.js
+++ sketch.js
@@ -6,13 +6,29 @@ function preload(){
 }
 
 function setup() {
-  createCanvas(600, 600);
-  video = createCapture(VIDEO);
+  createCanvas(windowWidth, windowHeight);
+  video = createCapture(VIDEO, {flipped: true});
   video.size(600,600);
   video.hide();
+  handPose.detectStart(video, gotHands);
 }
 
 function draw() {
   background(220);
-  //image(capture, 0, 0, 600, 600);
+  
+  for (let i = 0; i < hands.length; i++) {
+    let hand = hands[i];
+
+    let index = hand.keypoints[4];
+    let thumb = hand.keypoints[8];
+
+    fill(255, 0, 0);
+    noStroke();
+    circle(thumb.x, thumb.y, 15);
+    circle(index.x, index.y, 15);
+  }
+}
+
+function gotHands(results){
+  hands = results;
 }
